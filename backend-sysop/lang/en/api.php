241cbf1dbabec7da9b2fbda953c09b818cf7468a
danrdzprz <daniel.rpda@gmail.com>
2024-01-07 14:00:34 -0600

    feat: added request and lang files

---
+++ backend-sysop/lang/en/api.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+    |--------------------------------------------------------------------------
+    | App response messages line
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the api responses.
+    |
+    |
+    |
+    */
+
+return [
+    'auth' => [
+        'login' => [
+            'success' => 'User Logged in successfully.',
+            'failure' => 'Unauthorized',
+        ],
+        'logout' => [
+            'success' => 'User Logged out successfully.',
+        ],
+    ],
+    'authors' => [
+        'create' => [
+            'success' => 'Author registered successfully.',
+        ],
+    ],
+    'posts' => [
+        'create' => [
+            'success' => 'Post registered successfully.',
+        ],
+    ],
+];
